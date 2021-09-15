2f7b3db8de1feeb83fa195a2ce58075b29534dbf
Deco71 <73658534+Deco71@users.noreply.github.com>
2021-09-15 09:53:37 +0200

    Updated bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,14 @@
-print("Hello World");
+const Discord = require("discord.js")
+const client = new Discord.Client()
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on("message", msg => {
+  if (msg.content === "ping") {
+    msg.reply("pong");
+  }
+})
+
+client.login(process.env.TOKEN)
