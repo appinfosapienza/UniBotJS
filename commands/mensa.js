7ce76beb1fe23f4bf5c3d316e345d7f6a4b86090
Deco71 <pizzari4@gmail.com>
2021-11-04 21:28:50 +0100

    Update mensa.js
    Now the command works, yey
    
    Co-Authored-By: Shoenen <71129937+Shoenen@users.noreply.github.com>

--- commands/mensa.js
+++ commands/mensa.js
@@ -3,32 +3,54 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 const { baseEmbedGenerator } = require("../tools/baseEmbedFactory.js");
 const fs = require("fs");
 const axios = require("axios");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
 module.exports = {
   data: new SlashCommandBuilder().setName("mensa").setDescription("Yummy!"),
   async execute(interaction) {
     await interaction.deferReply();
-    await getHTML(
-      "http://www.cimasristorazione.com/menu-mense/universita-roma-1/"
-    );
+    baseEmbed = baseEmbedGenerator();
+    await getHTML();
     await interaction.followUp({ embeds: [baseEmbed] });
   },
 };
 //connect to a specific URL and return the HTML source using AXIOS
-async function getHTML(url) {
-  await axios
-    .get(url)
-    .then(function (response) {
-      let ret = response.data;
-      console.log(ret);
-      baseEmbed = baseEmbedGenerator();
-      baseEmbed.setTitle("Link ricevimenti dei professori");
-      baseEmbed.setDescription("Funziona");
-    })
+async function getHTML() {
+  const { data } = await axios
+    .get("http://www.cimasristorazione.com/menu-mense/universita-roma-1/")
     .catch(function (error) {
-      console.log(error);
-      baseEmbed = baseEmbedGenerator();
-      baseEmbed.setTitle("Link ricevimenti dei professori");
-      baseEmbed.setDescription("Errore bruh");
+      baseEmbed.setTitle("C'è stato un errore");
+      baseEmbed.setDescription("Come da titolo");
+      baseEmbed.setFooter("With an helping hand By GitHub Copilot");
     });
+  //create JSDOM object
+  const { document } = new JSDOM(data).window;
+  let day = getDay();
+  if (day != "pranzo-0") {
+    let roba = document.getElementById(day);
+    //convert HTMLDivElement to string
+    let html = roba.innerHTML;
+    let menu = html
+      .replace(/<h5>/g, "")
+      .replace(/<\/h5>/g, "\n")
+      .replace(/<ul>/g, "")
+      .replace(/<\/ul>/g, "\n")
+      .replace(/<li>/g, "-")
+      .replace(/<\/li>/g, "\n");
+    baseEmbed.setTitle("Ecco cosa ci sarà oggi a pranzo alla mensa");
+    baseEmbed.setDescription(menu);
+    baseEmbed.setFooter("With an helping hand By GitHub Copilot");
+  } else {
+    baseEmbed.setTitle("Ecco cosa ci sarà oggi a pranzo alla mensa");
+    baseEmbed.setDescription("Non c'è niente a pranzo");
+    baseEmbed.setFooter("With an helping hand By GitHub Copilot");
+  }
+}
+
+//get a number based on the day of the week
+function getDay() {
+  let d = new Date();
+  let day = d.getDay();
+  return "pranzo-" + day;
 }
