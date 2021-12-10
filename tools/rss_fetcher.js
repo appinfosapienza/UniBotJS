ceac85534ee5d9ecef38ea9fbc62df9d45fe4f8f
Deco71 <pizzari4@gmail.com>
2021-12-10 19:47:36 +0100

    Update rss_fetcher.js

--- tools/rss_fetcher.js
+++ tools/rss_fetcher.js
@@ -1,7 +1,9 @@
 let Parser = require("rss-parser");
+const {RSSLink} = require("../config.json");
 let parser = new Parser();
-const rssFeed = "https://lorem-rss.herokuapp.com/feed?unit=second&interval=30";
+//const rssFeed = "https://lorem-rss.herokuapp.com/feed?unit=second&interval=30";
 //const rssFeed = "https://www.gasparini.cloud/sapienza-feed";
+const rssFeed = RSSLink;
 
 module.exports.rss_fetcher = async () => {
 	let feed = await parser.parseURL(rssFeed);
