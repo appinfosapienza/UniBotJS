3637c247085c05e0b8f79e01d33b91e7b1c5815f
Deco71 <pizzari4@gmail.com>
2021-12-15 20:32:57 +0100

    Dai inutile che continuo a farli seri
    Voglio solo piangere

--- tools/rss_fetcher.js
+++ tools/rss_fetcher.js
@@ -1,11 +1,15 @@
 let Parser = require("rss-parser");
-const {RSSLink} = require("../config.json");
+const { RSSLink } = require("../config.json");
 let parser = new Parser();
 //const rssFeed = "https://lorem-rss.herokuapp.com/feed?unit=second&interval=30";
 //const rssFeed = "https://www.gasparini.cloud/sapienza-feed";
+//const empty = "https://lorem-rss.herokuapp.com/feed?length=0";
 const rssFeed = RSSLink;
 
 module.exports.rss_fetcher = async () => {
 	let feed = await parser.parseURL(rssFeed);
+	if (feed.items.length == 0) {
+		throw new Error("RSS Feed is empty!");
+	}
 	return feed;
 };
