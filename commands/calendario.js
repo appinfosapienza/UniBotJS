95aa02c1e1a2b8009fd2fdcd106f01036741b1b3
Deco71 <pizzari4@gmail.com>
2021-12-29 22:19:19 +0100

    Changed Calendario.js and Lezione.js

--- commands/calendario.js
+++ commands/calendario.js
@@ -6,12 +6,17 @@ module.exports = {
     .setName("calendario")
     .setDescription("Mostra il calendario delle lezioni"),
   async execute(interaction) {
+    /*
     baseEmbed = baseEmbedGenerator();
     baseEmbed.setTitle("Calendario");
     baseEmbed.setDescription(
       "**ATTENZIONE:**\n Il **lunedì** la prima lezione è quella di Sistemi Operativi (2 ore) \n Seguirà poi la lezione di Probabilità (3 ore)"
     );
     baseEmbed.setImage("https://i.imgur.com/kc2qetL.jpg");
+    */
+    baseEmbed = baseEmbedGenerator();
+    baseEmbed.setTitle("I corsi sono terminati");
+    baseEmbed.setDescription("Buona fortuna con gli esami!");
     await interaction.reply({ embeds: [baseEmbed] });
   },
 };
