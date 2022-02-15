651cf1098c2579aec39fff7c6682f6a5444700e2
Deco71 <pizzari4@gmail.com>
2022-02-15 10:51:34 +0100

    Added email command
    Co-Authored-By: Shoenen <71129937+Shoenen@users.noreply.github.com>

---
+++ commands/email.js
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { baseEmbedGenerator } = require("../tools/baseEmbedFactory.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("email")
+        .setDescription("Mostra le email dei docenti"),
+    async execute(interaction) {
+        let data = "";
+        try {
+            data = fs.readFileSync(
+                path.join(__dirname, "../resources/email.txt"),
+                "utf8"
+            );
+        } catch (err) {
+            console.error(err);
+        }
+        baseEmbed = baseEmbedGenerator();
+        baseEmbed.setTitle("Email");
+        baseEmbed.setDescription(data);
+        await interaction.reply({ embeds: [baseEmbed] });
+    },
+};
