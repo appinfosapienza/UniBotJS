6bdd5c4b22b02970891a1265549fb7db73609e45
dag7dev <dag7@protonmail.com>
2022-02-22 15:14:31 +0100

    index: jsondata is required

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const fs = require("fs");
 const { baseEmbedGenerator } = require("./tools/baseEmbedFactory.js");
 const { rss_sender } = require("./tools/rss_tools.js");
 const { formattedDate, saveDebug } = require("./tools/miscelaneous.js");
+jsonData = require('./resources/lezioni.json');
+
 let channel;
 let rss_feed = JSON;
 
